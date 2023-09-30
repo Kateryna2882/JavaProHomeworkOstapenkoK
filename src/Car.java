6d082a4ba0ef36e8092c0af59d0fdc3577c800c3
Kateryna2882 <111122172+Kateryna2882@users.noreply.github.com>
2023-09-30 16:15:26 +0300

    Add Car class

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    public static void start (){
+
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+    private static void startElectricity(){
+    }
+    private static void startCommand(){
+    }
+    private static void startFuelSystem(){
+
+    }
+}
